18dd53a7d5f1f314b43e3ad900784bd5799dbdb9
VeberAL <aleo83@rambler.ru>
2021-10-31 12:54:05 +0300

    Метод Цепочка ответственности

---
+++ src/ChainOfResponsibility/Notifier.java
@@ -0,0 +1,23 @@
+package ChainOfResponsibility;
+
+public abstract class Notifier {
+    private int priority;
+    private Notifier nextNotifier;
+
+    public Notifier(int priority){
+        this.priority = priority;
+    }
+
+    public void setNextNotifiet(Notifier nextNotifier) {
+        this.nextNotifier = nextNotifier;
+    }
+    public void notifyManager(String message, int level){
+        if(level >= priority){
+            write(message);
+        }
+        if(nextNotifier != null){
+            nextNotifier.notifyManager(message,level);
+        }
+    }
+    public abstract void write(String messge);
+}
