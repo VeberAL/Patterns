eb38f011f1f6867542d0ebbe00d646ff26f21053
VeberAL <aleo83@rambler.ru>
2021-11-03 12:36:34 +0300

    Метод Синглтон

---
+++ src/SingletonPattern/ProgramLogger.java
@@ -0,0 +1,22 @@
+package SingletonPattern;
+
+public class ProgramLogger {
+    private static  ProgramLogger programLogger;
+    private static String LogFile = "This is log file.\n\n";
+    public static ProgramLogger getProgramLogger(){
+        if (programLogger == null){
+            programLogger = new ProgramLogger();
+        }
+        return programLogger;
+    }
+    private ProgramLogger(){
+
+    }
+    public void addLogInfo(String logInfo){
+        LogFile += logInfo + "\n";
+    }
+    public void showLogFile(){
+        System.out.println(LogFile);
+    }
+}
+
