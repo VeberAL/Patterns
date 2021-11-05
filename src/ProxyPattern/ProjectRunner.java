db85df26cfa2e43fe5e6896a54553ec450d7bae7
VeberAL <aleo83@rambler.ru>
2021-11-05 16:31:41 +0300

    Метод Прокси

--- src/ProxyPattern/ProjectRunner.java
+++ src/ProxyPattern/ProjectRunner.java
@@ -3,6 +3,7 @@ package ProxyPattern;
 public class ProjectRunner {
     public static void main(String[] args) {
         Project project = new ProxyProject("https://www.github.com.VeberAL/Patterns");
+        //скачивание и запуск проекта происходит только после выполнения метода run
         project.run();
     }
 }
